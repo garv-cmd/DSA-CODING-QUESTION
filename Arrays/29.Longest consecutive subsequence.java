dab4a0f8694cabd33003b14b9400d8556ad3c193
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2020-11-22 20:55:58 +0530

    Create 29.Longest consecutive subsequence.java

---
+++ Arrays/29.Longest consecutive subsequence.java
@@ -0,0 +1,23 @@
+//https://practice.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1#
+static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   Set<Integer> set = new TreeSet<>();
+	   for(int i=0;i<N;i++){
+	       set.add(arr[i]);
+	   }
+	   int max=0,count=0;
+	   Iterator it = set.iterator();
+	   while (it.hasNext()) 
+	   {
+    int item = (int) it.next() + 1;
+    if (set.contains(item)) {
+    count++;
+    } else {
+    if (count > max)
+    max = count;
+    count = 0;
+}
+}
+	   return max+1;
+	}
