29067e9b2c972abbd8312d5f004f93de4ca3580a
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2020-12-01 22:50:52 +0530

    Update 15.Best time to buy and Sell stock.java

--- Arrays/15.Best time to buy and Sell stock.java
+++ Arrays/15.Best time to buy and Sell stock.java
@@ -2,20 +2,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int maxprofit = 0;
+       public class Solution {
+    public int maxProfit(int prices[]) {
         int minprice = Integer.MAX_VALUE;
-        for(int i=0;i<prices.length;i++){
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
             int profit = prices[i] - minprice;
-            if(prices[i] < minprice){
+            if (prices[i] < minprice)
                 minprice = prices[i];
-            }
-            
-            else if(profit > maxprofit){
-                maxprofit = prices[i] - minprice;
-            }
-//             minprice = Math.min(minprice,prices[i]);
-//             maxprofit = Math.max(maxprofit,prices[i]-minprice);
-            
+            else if (profit > maxprofit)
+                maxprofit = profit;
+        }
+        return maxprofit;
+    }
+}
         }
         return maxprofit;
         // int max = 0;
