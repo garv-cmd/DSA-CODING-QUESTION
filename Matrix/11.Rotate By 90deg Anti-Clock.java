2521f653233924777445519a1a6814e73e84228e
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2020-12-07 15:29:49 +0530

    Create 11.Rotate By 90deg Anti-Clock.java

---
+++ Matrix/11.Rotate By 90deg Anti-Clock.java
@@ -0,0 +1,22 @@
+class Solution{
+    static void rotateby90(int matrix[][], int n) 
+    { 
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int lo=0,hi=n-1;
+            while(lo<hi){
+                int t=matrix[lo][i];
+                matrix[lo][i]=matrix[hi][i];
+                matrix[hi][i]=t;
+                lo++;hi--;
+            }    
+        }
+        
+    }
