01ce2f038206b5827216b7edf88f4ce746f8ecda
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-01-10 18:20:17 +0530

    Create 00.Graph Implementation.java

---
+++ Graphs/00.Graph Implementation.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main{
+    public static class Edge{
+        int src;
+        int dest;
+        int wt;
+        Edge(int src,int dest,int wt){
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+     public static void main(String []args){
+        int vces = 7;
+        ArrayList<Edge>[]graph = new ArrayList[7];
+        for(int i=0;i<vces;i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0,3,40));
+        graph[0].add(new Edge(0,1,10));
+        
+        graph[1].add(new Edge(1,0,10));
+        graph[1].add(new Edge(1,2,10));
+        
+        graph[2].add(new Edge(2,3,10));
+        graph[2].add(new Edge(2,1,10));
+        
+        graph[3].add(new Edge(3,0,40));
+        graph[3].add(new Edge(3,2,40));
+        graph[3].add(new Edge(3,4,2));
+        
+        graph[4].add(new Edge(4,3,2));
+        graph[4].add(new Edge(4,5,3));
+        graph[4].add(new Edge(4,6,3));
+        
+        graph[5].add(new Edge(5,4,3));
+        graph[5].add(new Edge(5,6,3));
+        
+        graph[6].add(new Edge(6,5,3));
+        graph[6].add(new Edge(6,4,8));
+     }
+}
