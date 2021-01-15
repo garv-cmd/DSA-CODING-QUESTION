6cd6d83d61df31efb877279d71590340aefe734f
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-01-15 21:37:12 +0530

    Create 39.Bottom View of Binary Tree.java

---
+++ Binary Tree/39.Bottom View of Binary Tree.java
@@ -0,0 +1,44 @@
+//https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+class Tree
+{
+ 
+    // Method that returns the bottom view.
+     static class Pair{
+        Node node;
+        int hd;
+        Pair(Node node,int hd){
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+    public ArrayList <Integer> bottomView(Node root)
+    {
+    // Code here
+    ArrayList <Integer> al = new ArrayList <Integer>();
+    if(root==null)
+        return al;
+        Deque<Pair> q = new ArrayDeque<>();
+        Map<Integer,Integer> map = new TreeMap<>();
+        q.add(new Pair(root,0));
+        while(!q.isEmpty()){
+            Pair p = q.getFirst();
+            q.removeFirst();
+            Node temp = p.node;
+            int hd = p.hd;
+           
+                map.put(hd,temp.data);
+            
+            if(temp.left!=null){
+                q.add(new Pair(temp.left,hd-1));
+            }
+            
+            if(temp.right!=null){
+                q.add(new Pair(temp.right,hd+1));
+            }
+        }
+         for (int key : map.values()) {
+            al.add(key);
+        }
+        return al;
+    }
+}
