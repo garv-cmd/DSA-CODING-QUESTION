b49d82e0294c695ca864867590c93eecec1f7ab8
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-01-25 23:03:03 +0530

    Create 08.Hamiltonian Path And Cycle.java

---
+++ Graphs/08.Hamiltonian Path And Cycle.java
@@ -0,0 +1,89 @@
+/*
+1. You are given a graph and a src vertex.
+2. You are required to find and print all hamiltonian paths and cycles starting from src. The cycles must end with "*" and paths with a "."
+
+Note -> A hamiltonian path is such which visits all vertices without visiting any twice. A hamiltonian path becomes a cycle if there is an edge between first and last vertex.
+Note -> Print in lexicographically increasing order.
+
+7
+9
+0 1 10
+1 2 10
+2 3 10
+0 3 10
+3 4 10
+4 5 10
+5 6 10
+4 6 10
+2 5 10
+0
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList < Edge > [] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList < > ();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        int src = Integer.parseInt(br.readLine());
+
+        // write all your codes here
+        HashSet < Integer > visited = new HashSet < > ();
+        printHamiltonion(graph, src, visited, src + "", src);
+
+    }
+    public static void printHamiltonion(ArrayList < Edge > [] graph, int src, HashSet < Integer > visited, String psf, int osc) {
+        if (visited.size() == graph.length - 1) {
+            boolean check = false;
+            for (Edge e: graph[osc]) {
+                if (e.nbr == src) {
+                    check = true;
+                    break;
+                }
+            }
+            if (check) {
+                psf += "*";
+            } else psf += ".";
+            System.out.println(psf);
+            return;
+        }
+
+        visited.add(src);
+        for (Edge e: graph[src]) {
+            if (visited.contains(e.nbr) == false) {
+                printHamiltonion(graph, e.nbr, visited, psf + e.nbr, osc);
+            }
+        }
+        visited.remove(src);
+    }
+
+
+}
