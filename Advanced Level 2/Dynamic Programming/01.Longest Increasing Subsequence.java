48b29dfaab53e95459a8c99aa10e764b887617e8
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-03-02 19:21:53 +0530

    Create 01.Longest Increasing Subsequence.java

---
+++ Advanced Level 2/Dynamic Programming/01.Longest Increasing Subsequence.java
@@ -0,0 +1,18 @@
+Leetcode - 300
+  class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int dp[] = new int[nums.length];
+        int res=0;
+        for(int i=0;i<dp.length;i++){
+            int max=0;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    max = Math.max(max,dp[j]);
+                }
+            }
+            dp[i] = max+1;
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+    }
+}
