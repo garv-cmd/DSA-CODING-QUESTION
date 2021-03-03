29130abf7c543fb2330707724948a865eee7ab8b
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-03-03 23:07:30 +0530

    Create 08.Insert into a Binary Search Tree(leetcode).java

---
+++ Binary Search Tree/08.Insert into a Binary Search Tree(leetcode).java
@@ -0,0 +1,18 @@
+leetcode - 701
+  class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            root = new TreeNode(val);
+            return root;
+        }
+        if(val < root.val){
+            root.left = insertIntoBST(root.left,val);
+        }
+        
+        else if(val > root.val){
+            root.right = insertIntoBST(root.right,val);
+        }
+        return root;
+    }
+}
+  
