00d7496d4b566c98777fb476885b1abaf42d317b
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-03-07 23:44:51 +0530

    Update 33.Vertical Traversal of Binary Tree.java

--- Binary Tree/33.Vertical Traversal of Binary Tree.java
+++ Binary Tree/33.Vertical Traversal of Binary Tree.java
@@ -1,33 +1,43 @@
 //https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
 class BinaryTree
 {
-static class pair {
-Node root;
-int level ;
-pair(int l , Node root) {
-this.root = root;
-level = l;
-}
-}
-static ArrayList <Integer> verticalOrder(Node root)
-{
-    ArrayList <Integer> list = new ArrayList <>();
-    if(root == null) return list;
-        TreeMap<Integer , ArrayList<Integer>> map = new TreeMap<>();
-    Queue<pair> q = new LinkedList<>();
-    q.add(new pair(0 , root));
-while(!q.isEmpty()) {
-    pair p = q.remove();
-    
-    ArrayList <Integer> list1 = map.get(p.level);
-    if(list1 == null) list1 = new ArrayList <Integer>();
-        list1.add(p.root.data);
-    map.put(p.level , list1);
-    if(p.root.left != null) q.add(new pair(p.level - 1 , p.root.left));
-    if(p.root.right != null) q.add(new pair(p.level + 1 , p.root.right));
+    static class Pair{
+        Node node;
+        int hd;
+        Pair(Node node,int hd){
+            this.node = node;
+            this.hd = hd;
+        }
     }
-for(Map.Entry<Integer ,ArrayList<Integer>> entry : map.entrySet())
-list.addAll(entry.getValue());
-return list;
-}
+    static ArrayList <Integer> verticalOrder(Node root)
+    {
+        Queue<Pair> q = new LinkedList<>();
+       ArrayList<Integer>res = new ArrayList<>();
+        q.add(new Pair(root,0));
+        TreeMap<Integer,ArrayList<Integer>> map = new TreeMap<>();
+        while(q.size()>0){
+            Pair p = q.remove();
+            Node n = p.node;
+            int hd = p.hd;
+            if(map.containsKey(hd)){
+                ArrayList<Integer> a = map.get(hd);
+                a.add(n.data);
+                map.put(hd,a);
+            }else{
+                 ArrayList<Integer> a = new ArrayList<>();
+                a.add(n.data);
+                map.put(hd,a);
+            }
+            if(n.left!=null)
+                q.add(new Pair(n.left,hd-1));
+            
+            if(n.right!=null)
+                q.add(new Pair(n.right,hd+1));
+        }
+        for(int k : map.keySet()){
+            for(int val:map.get(k))
+            res.add(val);
+        }
+        return res;
+    }
 }
