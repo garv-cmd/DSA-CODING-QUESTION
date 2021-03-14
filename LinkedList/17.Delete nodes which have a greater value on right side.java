8eb55131ecb4f420934ea2abf1bad748f31f78d0
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2021-03-15 01:37:57 +0530

    Update 17.Delete nodes which have a greater value on right side.java

--- LinkedList/17.Delete nodes which have a greater value on right side.java
+++ LinkedList/17.Delete nodes which have a greater value on right side.java
@@ -1,18 +1,20 @@
 //https://practice.geeksforgeeks.org/problems/delete-nodes-having-greater-value-on-right/1#
-class GfG
+class Solution
 {
     Node compute(Node head)
     {
         // your code here
-        if(head.next == null)
+        Node temp = head;
+        while(temp!=null && temp.next!=null){
+            if(temp.data < temp.next.data){
+                temp.data=temp.next.data;
+                temp.next = temp.next.next;
+                temp=head;
+            }else{
+                temp=temp.next;
+            }
+        }
         return head;
-        
-       Node temp = compute(head.next);
-       if(temp.data > head.data){
-           return temp;
-       }else{
-           head.next = temp;
-       }
-       return head;
     }
 }
+  
